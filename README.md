4a59766e50827a2d0f1f7723ca1bc54e786aab25
Kostis Kapelonis <kostis@codefresh.io>
2021-07-22 16:03:26 +0300

    Changed settings

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ password and certificates.
 
 then visit http://localhost:8080 in your browser
 
-## How to build and run container
+## How to build and run the container
 
 Run
 
@@ -19,6 +19,19 @@ Run
  then visit http://localhost:8080 in your browser
 
 You can find prebuilt images at [https://hub.docker.com/r/kostiscodefresh/gitops-secrets-sample-app/tags](https://hub.docker.com/r/kostiscodefresh/gitops-secrets-sample-app/tags)
+
+## How to deploy to Kubernetes manually
+
+```
+cd manifests
+kubectl create ns gitops-secret-example
+kubectl apply -f . -n gitops-secret-example
+```
+
+## How to deploy with ArgoCD
+
+Fork this repository and then point ArgoCD to your own GitHub
+repo.
 
 See the [documentation page](https://codefresh.io/docs/docs/yaml-examples/examples/gitops-secrets/) for more details.
 
--- simple-web-server.go
+++ simple-web-server.go
@@ -8,19 +8,23 @@ import (
 )
 
 type configurationListHandler struct {
-	appMode         string
-	certificatePath string
-	paypalURL       string
-	dbUser          string
-	dbPassword      string
+	appMode        string
+	privateKeyPath string
+	publicKeyPath  string
+	paypalURL      string
+	paypalCertPath string
+	dbUser         string
+	dbPassword     string
 }
 
 func (h *configurationListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h2>I am a GO application running inside Kubernetes.<h2> <h3>My properties are:</h3><ul>")
 	fmt.Fprintf(w, "<li>app_mode: "+h.appMode+"</li>")
-	fmt.Fprintf(w, "<li>certificatePath: "+h.certificatePath+"</li>")
+	fmt.Fprintf(w, "<li>private_key: "+h.privateKeyPath+"</li>")
+	fmt.Fprintf(w, "<li>public_key: "+h.publicKeyPath+"</li>")
 	fmt.Fprintf(w, "<li>paypal_url: "+h.paypalURL+"</li>")
+	fmt.Fprintf(w, "<li>paypal_cert: "+h.paypalCertPath+"</li>")
 	fmt.Fprintf(w, "<li>db_user: "+h.dbUser+"</li>")
 	fmt.Fprintf(w, "<li>db_password: "+h.dbPassword+"</li>")
 	fmt.Fprintf(w, "</ol>")
@@ -36,21 +40,25 @@ func main() {
 
 	cfg, err := ini.LooseLoad("/config/settings.ini", "settings.ini")
 	if err != nil {
-		fmt.Printf("Fail to read configuration file: %v", err)
+		fmt.Printf("Failed to read configuration file: %v", err)
 	}
 
 	fmt.Println("App Mode:", cfg.Section("").Key("app_mode").String())
 
 	clh := configurationListHandler{}
 	clh.appMode = cfg.Section("").Key("app_mode").String()
-	clh.certificatePath = cfg.Section("security").Key("certificates").String()
+	clh.privateKeyPath = cfg.Section("security").Key("private_key").String()
+	clh.publicKeyPath = cfg.Section("security").Key("public_key").String()
 	clh.paypalURL = cfg.Section("paypal").Key("paypal_url").String()
+	clh.paypalCertPath = cfg.Section("paypal").Key("paypal_cert").String()
 	clh.dbUser = cfg.Section("mysql").Key("db_user").String()
 	clh.dbPassword = cfg.Section("mysql").Key("db_password").String()
 
 	fmt.Println("Simple web server is starting on port 8080...")
+
 	http.Handle("/", &clh)
 	http.HandleFunc("/health", healthHandler)
+
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Failed to start server at port 8080: %v", err)
